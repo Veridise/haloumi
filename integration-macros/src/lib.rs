072d6fa4503190e477e7495fa43814b32eb3d3f9
Daniel Dominguez <daniel@veridise.com>
2026-09-07 15:54:16 +0200

    Fix macro

--- integration-macros/src/lib.rs
+++ integration-macros/src/lib.rs
@@ -11,5 +11,9 @@ mod info_traits;
 /// Derive macro for the `SelectorInfo` trait.
 #[proc_macro_derive(SelectorInfo, attributes(haloumi))]
 pub fn derive_selector_info(input: TokenStream) -> TokenStream {
-    info_traits::derive_selector_info_impl(parse_macro_input!(input as DeriveInput)).into()
+    match info_traits::derive_selector_info_impl(parse_macro_input!(input as DeriveInput)) {
+        Ok(tok) => tok,
+        Err(e) => e.to_compile_error(),
+    }
+    .into()
 }
